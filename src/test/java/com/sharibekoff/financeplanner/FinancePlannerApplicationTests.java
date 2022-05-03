0b96d6af32068e2fd0d7919e4ac3e7abb78429d0
Muratbek Sharibekov <msharibekov@one.kz>
2022-05-03 21:56:14 +0600

    Initial setup

---
+++ src/test/java/com/sharibekoff/financeplanner/FinancePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharibekoff.financeplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
